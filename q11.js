91d36f99ea611f042aaad418618eb359cbd429cc
Udayasaran-kumar <ssaran3091998@gmail.com>
2024-12-02 21:04:11 +0530

    Update q11.js

---
+++ q11.js
@@ -0,0 +1,3 @@
+var Daily_Temperatures=["SUNDAY-24C","MONDAY-23C","TUESDAY-25C","WEDNESDAY-26C","THURSDAY-27C","FRIDAY-28C","SATURDAY-27C"];
+Daily_Temperatures[3]=="WEDNESDAY=28C";
+console.log(Daily_Temperatures);
