f007854396bb2953866ec9452b816686c5d1a1ea
Udayasaran-kumar <ssaran3091998@gmail.com>
2024-12-03 08:34:07 +0530

    Create q13.js

---
+++ q13.js
@@ -0,0 +1,10 @@
+var To_Do_List=["Check Mail","Greet Colleagues","Have Breakfast","Check Tasks","Complete Tasks"];
+To_Do_List[0]="";
+To_Do_List[5]=To_Do_List[4];
+To_Do_List[4]=To_Do_List[3];
+To_Do_List[3]=To_Do_List[2];
+To_Do_List[2]=To_Do_List[1];
+To_Do_List[1]="Open office early";
+To_Do_List[0]="Catch the bus on time";
+To_Do_List[5]="Report Manager";
+console.log(To_Do_List);
